15ca765c66360ec86ce25185cac68a12bd2f1903
chaz Graham <chazhg80.cg@gmail.com>
2022-12-12 20:46:54 -0600

    boiler plate code

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import Profile from './pages/Profile';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <React exact path='/profile' component={Profile} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Profile.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+
+import Auth from '../utils/auth';
+import { GET_ME } from '../utils/queries';
+
+const Profile = () => {
+    const [userData, setUserData] = useState({});
+
+    const userDataLength = Object.keys(userData).length;
+
+    useEffect(() => {
+        const getUserData = async () => {
+          try {
+            const token = Auth.loggedIn() ? Auth.getToken() : null;
+    
+            if (!token) {
+              return false;
+            }
+    
+            const response = await GET_ME(token);
+    
+            if (!response.ok) {
+              throw new Error('something went wrong!');
+            }
+    
+            const user = await response.json();
+            setUserData(user);
+          } catch (err) {
+            console.error(err);
+          }
+        };
+
+        getUserData();
+  }, [userDataLength]);
+
+   // if data isn't here yet, say so
+   if (!userDataLength) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+    user Profile data here
+    </>
+  )
+}
+
+export default Profile;
