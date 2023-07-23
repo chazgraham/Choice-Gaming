f2c27d0a958e5d96728804c94ca994a9f5a039cb
chaz Graham <chazhg80.cg@gmail.com>
2023-07-23 18:41:46 -0500

    restuctured header

--- client/src/components/Header/Navbar.js
+++ client/src/components/Header/Navbar.js
@@ -12,64 +12,57 @@ const AppNavbar = () => {
 
   return (
     <>
-      <Navbar variant='dark' expand='lg'>
-        <Container fluid>
-          <Navbar.Brand as={Link} to='/'>
-            <h1>Game Seeker</h1>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbar' />
-          <Navbar.Collapse id='navbar'>
-            <Nav className='ml-auto'>
-              <Nav.Link as={Link} to='/users'>
-                Veiw other Users
-              </Nav.Link>
-              {/* if user is logged in show saved games and logout */}
-              {Auth.loggedIn() ? (
-                <>
-                  <Nav.Link as={Link} to='/profile'>
-                    See Your games
-                  </Nav.Link>
-                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-                </>
-              ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      {/* set modal data up */}
-      <Modal
-        size='lg'
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby='signup-modal'>
-        {/* tab container to do either signup or login component */}
-        <Tab.Container defaultActiveKey='login'>
-          <Modal.Header closeButton>
-            <Modal.Title id='signup-modal'>
-              <Nav variant='pills'>
-                <Nav.Item>
-                  <Nav.Link eventKey='login'>Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey='login'>
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey='signup'>
-                <SignUpForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
+      <section className='header'>
+        <div>
+          <Link to='/'><h1 className='title'>Game Seeker</h1></Link>
+        </div>
+        <div>
+          <ul>
+            <Link to='/users'><li>find friends</li></Link>
+            {Auth.loggedIn() ? (
+              <>
+                <Link to='/profile'><li>Profile</li></Link>
+                <Link onClick={Auth.logout}><li>Logout</li></Link>
+              </>
+            ) : (
+              <Link onClick={() => setShowModal(true)}><li>Loggin/Sign Up</li></Link>
+            )}
+          </ul>
+        </div>
+      </section>
+      <section>
+        <Modal
+          size='lg'
+          show={showModal}
+          onHide={() => setShowModal(false)}
+          aria-labelledby='signup-modal'>
+          {/* tab container to do either signup or login component */}
+          <Tab.Container defaultActiveKey='login'>
+            <Modal.Header closeButton>
+              <Modal.Title id='signup-modal'>
+                <Nav variant='pills'>
+                  <Nav.Item>
+                    <Nav.Link eventKey='login'>Login</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Tab.Content>
+                <Tab.Pane eventKey='login'>
+                  <LoginForm handleModalClose={() => setShowModal(false)} />
+                </Tab.Pane>
+                <Tab.Pane eventKey='signup'>
+                  <SignUpForm handleModalClose={() => setShowModal(false)} />
+                </Tab.Pane>
+              </Tab.Content>
+            </Modal.Body>
+          </Tab.Container>
+        </Modal>
+      </section>
     </>
   );
 };
