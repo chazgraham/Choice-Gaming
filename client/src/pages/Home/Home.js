5d5ea8b490a7af858816a973d87a0510421b8264
chaz Graham <chazhg80.cg@gmail.com>
2023-08-26 21:59:41 -0500

    clear button styled

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -289,7 +289,7 @@ const Home = () => {
         <form className="search" onSubmit={handleFormSubmit}>
           <input className="searchTerm" type="text" placeholder="Search" value={searchedInput} onChange={(e) => setSearchInput(e.target.value)}></input>
           <button className="searchButton" type="submit"><i class="fa fa-search"></i></button>
-          <button onClick={(e) => setGameData([])}>clear</button>
+          <button className="clearButton" onClick={(e) => setGameData([])}>clear</button>
         </form>
         <div className="card_container">
           {gameData.map((game) => (
